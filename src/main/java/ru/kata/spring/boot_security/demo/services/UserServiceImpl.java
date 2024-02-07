b8a2676befd69d3c2e3afcd9f8ad3ca6e82eb7d9
Burkatskaya <anyaburkatskaya@gmail.com>
2024-02-07 17:02:30 +0300

    UserServiceImpl.java has been changed

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -0,0 +1,91 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.hibernate.Hibernate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.models.User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public User show(int id) {
+        User user = userRepository.getById(id);
+        Hibernate.initialize(user.getRoles());
+        return user;
+    }
+
+    @Override
+    @Transactional
+    public void create(User user) {
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void update(User updateUser) {
+        User user = userRepository.getById(updateUser.getId());
+        user.setUsername(updateUser.getUsername());
+        user.setLastname(updateUser.getLastname());
+        user.setPassword(updateUser.getPassword());
+        userRepository.save(updateUser);
+    }
+
+    @Override
+    @Transactional
+    public void delete(int id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null)  {
+            throw new UsernameNotFoundException(String.format("User %s not found!", username));
+        }
+
+//        return new ru.kata.spring.boot_security.demo.security.UserDetails(user);
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
+        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getAuthority())).collect(Collectors.toList());
+    }
+}
