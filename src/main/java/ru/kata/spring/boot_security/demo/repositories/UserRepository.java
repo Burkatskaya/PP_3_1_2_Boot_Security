c2f8d2a55b2144cb8573496f8c42e0d01b334fce
Burkatskaya <anyaburkatskaya@gmail.com>
2024-02-07 18:31:55 +0300

    UserRepository.java has been changed

--- src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
@@ -4,14 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.models.User;
 
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-//    List<User> listUsers();
     User findByUsername(String username);
-//    User show(int id);
-//    void create(User user);
-//    void update(User updateUser);
-//    void delete(int id);
 }
