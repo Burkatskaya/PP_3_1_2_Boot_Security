1d924532c243586316b1705f691cf98d895e69b1
Burkatskaya <anyaburkatskaya@gmail.com>
2024-02-07 18:31:46 +0300

    UserController.java has been changed

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -1,15 +1,9 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
 import java.security.Principal;
