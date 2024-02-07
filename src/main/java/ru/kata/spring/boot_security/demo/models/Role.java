95e85b4d5f54514d1398c1cce961ee56a10137f9
Burkatskaya <anyaburkatskaya@gmail.com>
2024-02-07 17:00:39 +0300

    Role.java has been changed

--- src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
@@ -1,4 +1,6 @@
-package ru.kata.spring.boot_security.demo.model;
+package ru.kata.spring.boot_security.demo.models;
+
+import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,13 +15,13 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "roles")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Id")
     private int id;
-    @Column(name = "Name", nullable = false)
-    private String name;
+    @Column(name = "Authority", nullable = false)
+    private String authority;
 
     @ManyToMany(mappedBy = "roles")
     private List<User> users;
@@ -28,9 +30,13 @@ public class Role {
 
     }
 
-    public Role(int id, String name) {
+    public Role(int id) {
         this.id = id;
-        this.name = name;
+    }
+
+    public Role(int id, String authority) {
+        this.id = id;
+        this.authority = authority;
     }
 
     public int getId() {
@@ -41,12 +47,12 @@ public class Role {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getAuthority() {
+        return authority;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setAuthority(String authority) {
+        this.authority = authority;
     }
 
     public List<User> getUsers() {
@@ -66,11 +72,11 @@ public class Role {
             return false;
         }
         Role role = (Role) o;
-        return id == role.id && Objects.equals(name, role.name);
+        return id == role.id && Objects.equals(authority, role.authority);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(id, authority);
     }
 }
