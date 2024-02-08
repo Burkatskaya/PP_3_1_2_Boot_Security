c5cee4eb56b23bfef27ce5e85f0ac923fa1eb298
Burkatskaya <anyaburkatskaya@gmail.com>
2024-02-08 16:56:42 +0300

    LoadUser.java was created

---
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/LoadUser.java
@@ -0,0 +1,41 @@
+package ru.kata.spring.boot_security.demo.configs;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import javax.annotation.PostConstruct;
+import java.util.Set;
+
+@Component
+public class LoadUser {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public LoadUser(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostConstruct
+    public void loadUsers() {
+        Role user = new Role("ROLE_USER");
+        Role admin = new Role("ROLE_ADMIN");
+
+        roleRepository.save(user);
+        roleRepository.save(admin);
+
+        User userUser = new User("user", "lastname1",
+                passwordEncoder.encode("user"), Set.of(user));
+        User adminUser = new User("admin", "lastname2",
+                passwordEncoder.encode("admin"), Set.of(admin));
+
+        userRepository.save(userUser);
+        userRepository.save(adminUser);
+    }
+}
