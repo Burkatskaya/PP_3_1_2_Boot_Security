a29aa9db0c1de9a9e79dac48a53bf33879772b1c
Burkatskaya <anyaburkatskaya@gmail.com>
2024-02-08 16:59:52 +0300

    UserServiceImpl.java has been changed

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -1,20 +1,14 @@
 package ru.kata.spring.boot_security.demo.services;
 
 import org.hibernate.Hibernate;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
-import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Service
 @Transactional(readOnly = true)
@@ -58,23 +52,5 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public void delete(int id) {
         userRepository.deleteById(id);
-    }
-
-    @Override
-    @Transactional
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
-        if (user == null)  {
-            throw new UsernameNotFoundException(String.format("User %s not found!", username));
-        }
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                mapRolesToAuthorities(user.getRoles()));
-    }
-
-    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
-        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getAuthority())).collect(Collectors.toList());
     }
 }
